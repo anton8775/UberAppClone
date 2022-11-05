49a3e78d26d224d4a054cbb3877df947665a9bb9
anton8775 <yacenko372o@gmail.com>
2022-11-05 22:07:38 +0200

    add map view action button

---
+++ UberAppClone/UberAppClone/Core/Home/Views/MapViewActionButton.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewActionButton.swift
+//  UberAppClone
+//
+//  Created by Антон Яценко on 05.11.2022.
+//
+
+import SwiftUI
+
+struct MapViewActionButton: View {
+    var body: some View {
+        Button{
+        } label: {
+            Image(systemName: "line.3.horizontal")
+                .font(.title2)
+                .foregroundColor(.black)
+                .padding()
+                .background(.white)
+                .clipShape(Circle())
+                .shadow(color: .black, radius: 6)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+struct MapViewActionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MapViewActionButton()
+    }
+}
